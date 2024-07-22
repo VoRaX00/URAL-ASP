91bd62db112e7f6b593c4e37f29961f9f12dce1a
S4MPAI <flekshum@gmail.com>
2024-07-22 14:01:39 +0500

    update controller

--- URAL/Controllers/UserController.cs
+++ URAL/Controllers/UserController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using URAL.Application.IServices;
 using URAL.Application.RequestModels.User;
+using URAL.Authentication;
+using URAL.Domain.Entities;
 
 namespace URAL.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UserController(IUserService service) : ControllerBase
+public class UserController(AuthOptions authOptions, IUserService service) : ControllerBase
 {
     [HttpGet("{id}")]
     public ActionResult<UserToGet> Get([FromRoute] ulong id)
@@ -16,10 +19,17 @@ public class UserController(IUserService service) : ControllerBase
     }
 
     [HttpPost]
-    public async Task<ActionResult<ulong>> Add([FromBody] UserToAdd userToAdd)
+    public async Task<ActionResult> Register([FromBody] UserToAdd userToAdd)
     {
         var entityId = await service.AddAsync(userToAdd);
         return entityId;
+    }
+
+    public async Task<ActionResult> ConfirmEmail(string userId, string code)
+    {
+        if (userId == null || code == null)
+            return BadRequest();
+        return Ok();
     }
 
     [HttpDelete("{id}")]
