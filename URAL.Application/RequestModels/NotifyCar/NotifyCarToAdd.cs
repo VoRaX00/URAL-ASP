65ad6603adaee95f63d72c1af0e04a7077b43efd
S4MPAI <flekshum@gmail.com>
2024-07-31 23:08:03 +0500

    update NotifyToAdd

--- URAL.Application/RequestModels/NotifyCar/NotifyCarToAdd.cs
+++ URAL.Application/RequestModels/NotifyCar/NotifyCarToAdd.cs
@@ -4,8 +4,6 @@ namespace URAL.Application.RequestModels.NotifyCar;
 
 public record NotifyCarToAdd
 {
-    public char FirstUserStatus { get; init; } = UserStatus.Yes;
-    public char SecondUserStatus { get; init; } = UserStatus.Unknown;
     public string? FirstUserComment { get; init; }
     public string? SecondUserComment { get; init; }
     public ulong CarId { get; init; }
--- URAL.Application/RequestModels/NotifyCargo/NotifyCargoToAdd.cs
+++ URAL.Application/RequestModels/NotifyCargo/NotifyCargoToAdd.cs
@@ -4,8 +4,6 @@ namespace URAL.Application.RequestModels.NotifyCargo;
 
 public record NotifyCargoToAdd
 {
-    public char FirstUserStatus { get; init; } = UserStatus.Yes;
-    public char SecondUserStatus { get; init; } = UserStatus.Unknown;
     public string? FirstUserComment { get; init; }
     public string? SecondUserComment { get; init; }
     public ulong CargoId { get; init; }
